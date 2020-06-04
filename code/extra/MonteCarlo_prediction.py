d6f2a37632d7e73217ff11c2692d523e0aaf41b1
Luca Clissa <luca.clissa2@unibo.it>
2020-06-04 11:55:30 +0200

    Implement both final and incremental updates Monte Carlo policy evaluation.

--- code/extra/MonteCarlo_prediction.py
+++ code/extra/MonteCarlo_prediction.py
@@ -2,7 +2,7 @@ def MC_policy_evaluation(policy, MC_type):
     pass
 
 
-def mc_state_policy_evaluation(state, mc_type, episodes_path):
+def mc_state_policy_evaluation_final_update(state, mc_type, episodes_path):
     """
     Evaluate a policy using final-update Monte Carlo for the specified state reading episodes from .csv files.
     :param state: the state for which the value function is computed
@@ -15,9 +15,6 @@ def mc_state_policy_evaluation(state, mc_type, episodes_path):
 
     # initialize state counter
     n_state = 0
-
-    # initialize value function
-    v_state = -1
 
     # initialize episode return
     g_t = 0
@@ -53,6 +50,63 @@ def mc_state_policy_evaluation(state, mc_type, episodes_path):
 
     # update value function
     # TODO implement non-stationary version with alpha weight
-    v_state += 1/n_state * (g_t - v_state)
+    v_state = g_t / n_state
+
+    return v_state
+
+
+def mc_state_policy_evaluation_incremental(state, mc_type, episodes_path):
+    """
+    Evaluate a policy using incremental-update Monte Carlo for the specified state reading episodes from .csv files.
+    :param state: the state for which the value function is computed
+    :param mc_type: Monte Carlo type (either \"first\" or \"every\")
+    :param episodes_path: path where the episode for the fixed policy are stored
+    :return: value function estimate for the input state
+    """
+    from pathlib import Path
+    import pandas as pd
+
+    # initialize state counter
+    n_state = 0
+
+    # initialize value function
+    v_state = 0
+
+    for path in Path(episodes_path).iterdir():
+        # read episode history
+        episode_history = pd.read_csv(path, index_col="time")
+
+        # replace literals with numerical values
+        episode_history = episode_history.replace(["terminal", "draw"], [22, -1]).astype(
+            {"dealer_score": int, "player_score": int})
+
+        state_occurences = episode_history[
+            (episode_history.dealer_score == state.dealer_score) & (episode_history.player_score == state.player_score)]
+
+        # initialize episode return
+        g_t = 0
+
+        if state_occurences.shape[0] == 0:
+            continue
+        elif mc_type == "first":  # first-visit MC
+            # update state frequency counter
+            n_state += 1
+
+            # compute return
+            first_occurence_time = state_occurences.index[0]
+            g_t += episode_history[first_occurence_time:].reward.sum()
+        elif mc_type == "every":
+            # update state frequency counter
+            n_state += state_occurences.shape[0]
+
+            # compute return
+            for first_occurence_time in state_occurences.index:
+                g_t += episode_history[first_occurence_time:].reward.sum()
+
+        # update value function
+        # TODO implement non-stationary version with alpha weight
+        v_state += 1/n_state * (g_t - v_state)
+        print(v_state, n_state, g_t)
+        # v_state = g_t / n_state
 
     return v_state
